ddc3257283ddb52d02f81b933cd08d32bae2a6ec
Rishika Sharma <rishikaosharma@gmail.com>
2024-09-05 18:10:59 +0530

    Add files via upload

---
+++ Aggregating Functions.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([5,10,15,20,25,30,35])
+print("Sum", np.sum(arr))
+print("Minimum", np.min(arr))
+print("Maximum", np.max(arr))
+print("Size", np.size(arr))
+print("Mean", np.mean(arr))
+print("Median", np.median(arr))
+print("Cummilative sum", np.cumsum(arr))
+print("Cummilative product", np.cumprod(arr))
