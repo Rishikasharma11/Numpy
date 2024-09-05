2a991e2ce2f68fb1322c29d04a39e87ff34c8069
Rishika Sharma <rishikaosharma@gmail.com>
2024-09-05 16:11:28 +0530

    Add files via upload

---
+++ Array Sort.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# Sorting 1-D Array(integer)
+arr = np.array([3, 5, 2, 9, 0, 5, 7, 8])
+newarr = np.sort(arr)
+print(newarr)
+
+# Sorting 1-D Array(characters)
+arr = np.array(['r', 'i', 's', 'h', 'i', 'k', 'a'])
+newarr = np.sort(arr)
+print(newarr)
+
+# Sorting 1-D Array(string)
+arr = np.array(['Rishika','Mohit', 'Rohit', 'Siya'])
+newarr = np.sort(arr)
+print(newarr)
+
+# Sorting 1-D Array(Boolean)
+arr = np.array([True, False, True, False, True])
+newarr = np.sort(arr)
+print(newarr)
+
+# Sorting 2-D Array  
+arr = np.array([[4, 2, 7, 0], [5, 9, 2, 4]])
+newarr = np.sort(arr)
+print(newarr)
+
