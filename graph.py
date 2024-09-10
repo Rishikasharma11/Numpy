9bed50f98ad65c64028853a99de4b88a094b8552
Rishika Sharma <rishikaosharma@gmail.com>
2024-09-10 18:30:30 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.distplot([0, 1, 2, 3, 4])  #for older version use distplot and for latest use displot
+plt.show()
+
+# using hist = false
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.distplot([0, 1, 2, 3, 4, 5], hist = False)  #hist is the histogram
+plt.show()
