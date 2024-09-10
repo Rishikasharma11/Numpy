66801c75ddb4f8d2a29ecea774d9f6aae482948b
Rishika Sharma <rishikaosharma@gmai.com>
2024-09-10 12:47:42 +0530

    seaborn

--- Data Distribution.py
+++ Data Distribution.py
@@ -1,3 +1,5 @@
+# The sun of the probabilty will not be more than to 1. 
+# The one with higher probability will be printed
 from numpy import random
 x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100))
 print(x)
